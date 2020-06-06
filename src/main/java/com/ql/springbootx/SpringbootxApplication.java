f71dcc3285991dc61c05689153dc4aebb9c981aa
ql46042 <ql46042@ly.com>
2020-06-06 18:20:39 +0800

    project create initial

---
+++ src/main/java/com/ql/springbootx/SpringbootxApplication.java
@@ -0,0 +1,13 @@
+package com.ql.springbootx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ql/springbootx/SpringbootxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ql.springbootx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
